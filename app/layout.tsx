4f943360b5808d2e0b6c7eaac5037793905ccf75
Huntergs16 <samoyhun@msu.edu>
2023-06-08 18:39:50 -0500

    added cute logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -16,11 +15,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <body className={inter.className}>
         {children}
         <footer className="text-center">
--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
     images: {
         domains: ['frontend-take-home.fetch.com'],
       },
+        // Define the `exportPathMap` function
+      exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
+        return {
+          // Add the path for the favicon
+          '/favicon.ico': { page: '/favicon.ico' },
+          // Add any other paths you need to map
+          // ...
+          ...defaultPathMap,
+        };
+      },
 }
 
 module.exports = nextConfig
