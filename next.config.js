332da1246cea8b4312267be993ca7c0a30b73329
Huntergs16 <samoyhun@msu.edu>
2023-06-08 18:42:09 -0500

    got rid of exportPathMap

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
     images: {
         domains: ['frontend-take-home.fetch.com'],
       },
-        // Define the `exportPathMap` function
-      exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-        return {
-          // Add the path for the favicon
-          '/favicon.ico': { page: '/favicon.ico' },
-          // Add any other paths you need to map
-          // ...
-          ...defaultPathMap,
-        };
-      },
 }
 
 module.exports = nextConfig
